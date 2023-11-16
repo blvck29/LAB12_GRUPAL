dda0415bd46fd6fc9a69e88878511aedceb5450f
blvck29 <a20210850@pucp.edu.pe>
2023-11-16 09:28:56 -0500

    a

--- rougeClans/src/main/java/com/game/rougeclans/model/beans/Persona.java
+++ rougeClans/src/main/java/com/game/rougeclans/model/beans/Persona.java
@@ -3,6 +3,17 @@ package com.game.rougeclans.model.beans;
 public class Persona {
 
     private int idPersona;
-
+    private Civilizacion civilizacion;
+    private String rol;
+    private String nombre;
+    private String genero;
+    private int alimentoDia;
+    private int morial;
+    private int fuerza;
+    private int produce;
+    private int exiliado;
+    private int alimentado;
+    private int daysAlive;
+    private String muerte;
 
 }
